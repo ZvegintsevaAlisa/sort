758913869cb74eb4f80300e6c62cc4a33f7561d7
ZvegintsevaAlisa <102322490+ZvegintsevaAlisa@users.noreply.github.com>
2022-03-24 22:31:38 +0300

    Add files via upload

---
+++ heap.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void heap(int* A, int n, int i)
+{
+    int max = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    if (l < n && A[l] > A[max])
+        max = l;
+    if (r < n && A[r]>A[max])
+        max = r;
+    if (max != i)
+    {
+        swap(A[i], A[max]);
+        heap(A, n, max);
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int n;
+    cout << "Число элементов:";
+    cin >> n;
+    int* A = new int[n];
+
+
+    cout << "Массив:";
+    for (int i = 0; i < n; i++)
+        cin >> A[i];
+
+    for (int i = n / 2; i >= 0; i--)
+        heap(A, n-1, i);
+    for (int i = n - 1; i >= 1; i--)
+    {
+        swap(A[0], A[i]);
+        heap(A, i-1, 0);
+    }
+
+    cout << "Отсортированный массив:";
+    for (int i = 0; i < n; i++)
+        cout << A[i] << " ";
+    return 0;
+}
